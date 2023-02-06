9e3ef22f90847078fa72da0906d05854eff2646d
melvingachet <114407271+Melou233@users.noreply.github.com>
2023-02-06 16:56:10 +0100

    reservation
    layout reservation

---
+++ app/src/main/java/com/example/module2/Reservation.kt
@@ -0,0 +1,11 @@
+package com.example.module2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reservation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservation)
+    }
+}
