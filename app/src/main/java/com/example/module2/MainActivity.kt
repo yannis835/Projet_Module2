7212d1902c067842cd6fd702360fbe2bc2b4d7b2
SCHEMS-ELDIN MAKHLOUF <schems.makhlouf@isen.yncrea.fr>
2023-02-07 14:49:56 +0100

    home

--- app/src/main/java/com/example/module2/MainActivity.kt
+++ app/src/main/java/com/example/module2/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.IgnoreExtraProperties
@@ -24,12 +27,19 @@ class MainActivity : AppCompatActivity() {
         buttonListener()
         database = Firebase.database("https://module2androidstudio-default-rtdb.europe-west1.firebasedatabase.app/").reference
         writeNewUser()
+
+        val buttonSignIn1 = findViewById<Button>(R.id.buttonSignIn1)
+        buttonSignIn1.setOnClickListener{
+            val intent= Intent(this,Login::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
         super.onStart()
         Log.d("MainActivity", "onStart")
     }
+
     override fun onResume() {
         super.onResume()
         Log.d("MainActivity", "onResume")
@@ -58,11 +68,13 @@ class MainActivity : AppCompatActivity() {
             showCategory(Category.CALENDAR)
         }
     }
+
     private fun showCategory(category: Category){
         val intent = Intent( this, CalendarActivity::class.java)
         intent.putExtra(CalendarActivity.extraKey, category)
         startActivity(intent)
     }
+
 }
 @IgnoreExtraProperties
 data class User(val username: String? = null, val email: String? = null) {
