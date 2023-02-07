ba818071ce2130baeafbf9d303ede573909df02f
melvingachet <114407271+Melou233@users.noreply.github.com>
2023-02-07 11:53:36 +0100

    Calendrier

--- app/src/main/java/com/example/module2/CalendarActivity.kt
+++ app/src/main/java/com/example/module2/CalendarActivity.kt
@@ -1,20 +1,31 @@
 package com.example.module2
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
 import com.example.module2.databinding.ActivityCalendarBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 enum class Category {CONNECTION,CALENDAR,INFORMATIONS}
 
 
 class CalendarActivity : AppCompatActivity() {
 
+
     companion object{
         const val extraKey ="extraKey"
     }
     lateinit var binding:ActivityCalendarBinding
     lateinit var currentCategory : Category
-
+    var button_date: Button? = null
+    var textview_date: TextView? = null
+    var cal = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         binding = ActivityCalendarBinding.inflate(layoutInflater)
@@ -22,6 +33,32 @@ class CalendarActivity : AppCompatActivity() {
         val category =intent.getSerializableExtra(extraKey) as? Category
         currentCategory=category ?: Category.CONNECTION
         supportActionBar?.title = categoryName()
+        textview_date = this.binding.textViewDate1
+        button_date = this.binding.buttonDate1
+
+        textview_date!!.text = "--/--/----"
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(
+                view: DatePicker, year: Int, monthOfYear: Int,
+                dayOfMonth: Int
+            ) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+            button_date!!.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(view: View) {
+                    DatePickerDialog(this@CalendarActivity,
+                        dateSetListener,
+                        // set DatePickerDialog to point to today's date when it loads up
+                        cal.get(Calendar.YEAR),
+                        cal.get(Calendar.MONTH),
+                        cal.get(Calendar.DAY_OF_MONTH)).show()
+                }
+
+            })
     }
 
     private fun categoryName(): String{
@@ -30,6 +67,11 @@ class CalendarActivity : AppCompatActivity() {
             Category.CALENDAR-> "Calendar"
             Category.INFORMATIONS->"Informations"
         }
+    }
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        textview_date!!.text = sdf.format(cal.getTime())
     }
 
 }
